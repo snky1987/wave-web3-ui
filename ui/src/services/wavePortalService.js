7a42a9cd19147937e6b20f8c90232905a84be5f7
Michal Malinowski <michal.m@logically.co.uk>
2022-08-09 23:43:28 +0100

    debugging

--- ui/src/services/wavePortalService.js
+++ ui/src/services/wavePortalService.js
@@ -3,6 +3,11 @@ import WavePortalContract from '../utils/WavePortal.json';
 
 export const getWavePortalService = (ethereum) => {
     const web3Provider = new providers.Web3Provider(ethereum);
+
+    console.log({
+        env: process.env
+    });
+
     const wavePortalContract = new Contract(process.env.REACT_APP_CONTRACT_KEY, WavePortalContract.abi, web3Provider.getSigner());
 
     return ({
