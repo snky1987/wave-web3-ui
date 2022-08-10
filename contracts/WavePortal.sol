c10065be6e6df155b488284b9cec7668df521ccf
Michal Malinowski <michal.m@logically.co.uk>
2022-08-10 11:23:32 +0100

    Added prize, events and safety checks

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -8,12 +8,47 @@ contract WavePortal {
         address sender;
     }
 
+    event NewMessage(address indexed from, uint256 timestamp, string message);
+    event NewPrize(address indexed from, uint256 timestamp);
+
     Input[] private inputs;
+    mapping(address => bool) public uniqueMessagesRegistry;
+    uint256 private seed;
+    uint256 private probability = 10;
+    uint256 prizeAmount = 0.0001 ether;
 
-    constructor() {}
+    constructor() payable {
+        seed = (block.timestamp + block.difficulty) % 100;
+    }
 
-    function createInput(string calldata text) public {
-        inputs.push(Input(text, msg.sender));
+    function createInput(string calldata _message) public {
+        require(uniqueMessagesRegistry[msg.sender] != true, "Already sent the message");
+
+        inputs.push(Input(_message, msg.sender));
+        uniqueMessagesRegistry[msg.sender] = true;
+
+        seed = (block.difficulty + block.timestamp + seed) % 100;
+
+        if (checkUserWon()) {
+            sendPrize();
+        }
+
+        emit NewMessage(msg.sender, block.timestamp, _message);
+    }
+
+    function checkUserWon() private view returns (bool) {
+        return ((block.difficulty + block.timestamp + seed) % 100) <= probability;
+    }
+
+    function sendPrize() private {
+        require(
+            prizeAmount <= address(this).balance,
+            "Trying to withdraw more money than the contract has."
+        );
+        (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+        require(success, "Failed to withdraw money from contract.");
+
+        emit NewPrize(msg.sender, block.timestamp);
     }
 
     function getInputs() public view returns (Input[] memory) {
--- scripts/run.js
+++ scripts/run.js
@@ -1,17 +1,25 @@
 const main = async () => {
     const [owner, randomPerson] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-    const waveContract = await waveContractFactory.deploy();
+    const waveContract = await waveContractFactory.deploy({
+        value: hre.ethers.utils.parseEther("0.1"),
+    });
     await waveContract.deployed();
 
     console.log("Contract deployed to:", waveContract.address);
     console.log("Contract deployed by:", owner.address);
+    let contractBalance = await hre.ethers.provider.getBalance(
+        waveContract.address
+    );
+    console.log("Contract balance:", hre.ethers.utils.formatEther(contractBalance));
 
     await waveContract.createInput("Hello you");
     await waveContract.connect(randomPerson).createInput("Hello you again");
+    contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
 
-    console.log("We have %d total inputs!", await waveContract.getTotalInputs())
-    console.log("Here are the inputs:", await waveContract.getInputs())
+    console.log("We have %d total inputs!", await waveContract.getTotalInputs());
+    console.log("Here are the inputs:", await waveContract.getInputs());
+    console.log("Contract balance:", hre.ethers.utils.formatEther(contractBalance));
 };
 
 const runMain = async () => {
